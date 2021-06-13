bdab256e0551c076bc2b9e37af658fcc68314e12
Antonio King <antonio.j.king@gmail.com>
2021-06-13 14:49:21 -0600

    Create encrypt method for enigma class; incomplete

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,4 +6,26 @@ describe Enigma do
     enigma = Enigma.new
     expect(enigma).to be_an(Enigma)
   end
+
+  it 'encrypt' do
+    enigma = Enigma.new
+    message = 'hello world'
+    key = '02715'
+    date = '040895'
+
+    expected1 = {
+      encryption: 'keder ohulw',
+      key:        '02715',
+      date:       '130621'
+    }
+
+    expected2 = {
+      encryption: 'keder ohulw',
+      key:        '02715',
+      date:       '040895'
+    }
+
+    expect(enigma.encrypt(message, key)).to eq(expected1)
+    expect(enigma.encrypt(message, key, date)).to eq(expected2)
+  end
 end
